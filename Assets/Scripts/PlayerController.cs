ed851f23560c481784d73d5412cb3f03ae48c6e2
Lucy Snyper <ls17545@essex.ac.uk>
2020-11-13 17:44:44 +0000

    Update on files
    Part 1 update

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent < Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horAxis = Input.GetAxis("Horizontal");
+        float verAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(verAxis, 0.0f, horAxis);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}

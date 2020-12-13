df63ba298560824be625f6fe20372df31befd0fa
Lucy Snyper <ls17545@essex.ac.uk>
2020-12-13 19:18:31 +0000

    Enemy Added

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Enemy variables")]
+    public bool PlayerSighted;
+    public bool EnemyBite;
+    public GameObject Player;
+    public float Speed;
+    public int MinDis;
+    public int MaxDis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerSighted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerSighted == true)
+            PlayerFound();
+    }
+
+    void PlayerFound()
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) >= MinDis) {
+            move();
+            if (Vector3.Distance(transform.position, Player.transform.position) <= MaxDis) {
+                EnemyBite = true;
+            }
+        }
+
+
+    }
+
+    void move()
+    {
+        transform.LookAt(Player.transform);
+        // enemy moves forward
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == Player) {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.transform == Player) {
+
+            PlayerSighted = true;
+            
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == Player) {
+            PlayerSighted = false;
+        }
+    }
+
+}

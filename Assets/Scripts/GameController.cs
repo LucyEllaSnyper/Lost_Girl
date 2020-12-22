fff9140b0a08207ef55924dfbe3ab6248581c197
Lucy Snyper <ls17545@essex.ac.uk>
2020-12-22 08:00:17 +0000

    update
    StartScreen & Character update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
